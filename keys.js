c1b0697871e0ffc57cd5ac0adc3e682902e7e879
Marsh <marshm@oit.state.co.us>
2018-02-02 13:18:36 -0700

    friday midday

--- keys.js
+++ keys.js
@@ -1,15 +1,17 @@
 console.log('Loaded Successfully!')
 
-var twitKey = new Twitter({
-    consumer_key:        '5iFNDxwP22CH6qErtvM1pktBE',
-    consumer_secret:     'NNiA4zB7xYi5jU4hfHfC8HJ7SacXdJGaBm0nd6sWKaJNE19Ieu',
-    access_token_key:    '958737189839319041-bpVZFwBQL7pE4AutwcrA2UeRgPUejhR'
-    ,
-    access_token_secret: 't9csFErS13NCdZ6s2esHfnyVof2kJi5aPQHEPxxTzayMs'
-});
+var twitKey={
+    consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key:    process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
 
-var spotKey = new Spotify({
-    id:     '52f4215b9f304705ab919d70dbba9249',
-    secret: '10e54d8b89a8440180f63ce4671e972d'
-});
+var spotKey={
+    id:     process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+module.exports={ spotify: spotKey,
+                  twitter: twitKey};
 
--- liri.js
+++ liri.js
@@ -1,10 +1,53 @@
+require("dotenv").config();
 var stuffINeed = require("./keys.js");
-
-console.log(stuffINeed)
-
 var request = require('request');
-request('http: //www.google.com', function (error, response, body) {
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+
+
+
+console.log(stuffINeed.spotify.secret);
+
+/**var client = new Twitter({
+    consumer_key: stuffINeed.twitter.consumer_key,
+    consumer_secret: stuffINeed.twitter.consumer_secret,
+    access_token_key: stuffINeed.twitter.access_token_key,
+    access_token_secret: stuffINeed.twitter.access_token_secret
+  });
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+request('http://www.google.com', function (error, response, body) {
+    //run JSON.parse(body);
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
-}); 
+}); **/
+var arg1 = process.argv[2];
+
+switch(arg1){
+    case 'mytweets':
+    var client = new Twitter({
+        consumer_key: stuffINeed.twitter.consumer_key,
+        consumer_secret: stuffINeed.twitter.consumer_secret,
+        access_token_key: stuffINeed.twitter.access_token_key,
+        access_token_secret: stuffINeed.twitter.access_token_secret
+      });
+    
+    var params = {screen_name: 'nodejs'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets);
+      }
+    });
+    break;
+    case 'spotifyThis':
+    console.log('spotify logic');
+    break;
+}
+
