65f8245bbd9658968c0a4d453eb1fa3169460b4f
Matthew <matthewmarshdev@gmail.com^C>
2018-02-03 22:56:47 -0700

    sat night commit

--- liri.js
+++ liri.js
@@ -4,50 +4,31 @@ var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
 
 
-/**var client = new Twitter({
+var theTwitter = function(){
+
+var client = new Twitter({
     consumer_key: stuffINeed.twitter.consumer_key,
     consumer_secret: stuffINeed.twitter.consumer_secret,
     access_token_key: stuffINeed.twitter.access_token_key,
     access_token_secret: stuffINeed.twitter.access_token_secret
   });
 
-var params = {screen_name: 'nodejs'};
+var params = {screen_name: 'MMLiri18'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+  if (!error && arg1 === 'my-tweets') {
     console.log(tweets);
   }
 });
-
-request('http://www.google.com', function (error, response, body) {
-    //run JSON.parse(body);
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-}); **/
-var arg1 = process.argv[2];
-
-var theTwitter = function(){
-
-            var client = new Twitter({
-              consumer_key: stuffINeed.twitter.consumer_key,
-              consumer_secret: stuffINeed.twitter.consumer_secret,
-              access_token_key: stuffINeed.twitter.access_token_key,
-              access_token_secret: stuffINeed.twitter.access_token_secret
-            });
-
-          var params = {screen_name: 'MMLiri18'};
-          client.get('search/tweets', params, function(error, tweets, response) {
-              if (!error) {
-                console.log(tweets);
-              }
-          });
 }
+
 
 var theSpotify = function(){
 
-          var trackSearch = arg1
+          var trackSearch = arg2
 
           var spotify = new Spotify({
             id: stuffINeed.spotify.access_token_key,
@@ -59,24 +40,26 @@ var theSpotify = function(){
                 return console.log('Error occurred: ' + err);
               }
           
-          console.log(data); 
+          console.log(data.name); 
           });
         }
 
 var omdbReq = function(){
-
-          request('http://www.omdb.com', function (error, response, body) {
+      
+      if(arg1 === 'movie-this'){
+          var movieSearch = 'http://www.omdbapi.com/?apikey=trilogy&t='+ arg2;
+          request(movieSearch, function (error, response, body) {
             //run JSON.parse(body);
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
         });
 
-
+      }
 }
 
 
-switch(arg1){
+/**switch(arg1){
     case 'mytweets': 
     theTwitter();
     break;
@@ -88,4 +71,4 @@ switch(arg1){
     case 'findMovie':
     omdbReq();
     break;
-}
+}**/
