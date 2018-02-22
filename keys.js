859f92152f54dcedf58605affcc694a846c428fb
Matthew <matthewmarshdev@gmail.com^C>
2018-02-21 18:34:17 -0700

    newpush after tutoring!!!

--- keys.js
+++ keys.js
@@ -1,17 +1,15 @@
-console.log('Loaded Successfully!')
 
-var twitKey={
+
+exports.twitKey={
     consumer_key:        process.env.TWITTER_CONSUMER_KEY,
     consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
     access_token_key:    process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-var spotKey={
+exports.spotKey={
     id:     process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
 
-module.exports={ spotify: spotKey,
-                  twitter: twitKey};
 
--- liri.js
+++ liri.js
@@ -7,22 +7,24 @@ var fs = require("fs");
 
 var arg1 = process.argv[2];
 var arg2 = process.argv[3];
-
-
+ 
 
 
 var theTwitter = function(){
 
         var client = new Twitter({
-            consumer_key: stuffINeed.twitter.consumer_key,
-            consumer_secret: stuffINeed.twitter.consumer_secret,
-            access_token_key: stuffINeed.twitter.access_token_key,
-            access_token_secret: stuffINeed.twitter.access_token_secret
+            consumer_key: stuffINeed.twitKey.consumer_key,
+            consumer_secret: stuffINeed.twitKey.consumer_secret,
+            access_token_key: stuffINeed.twitKey.access_token_key,
+            access_token_secret: stuffINeed.twitKey.access_token_secret
           });
 
         var params = {screen_name: 'MMLiri18'};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
-         
+          for(var i=0;i<10;i++){
+            console.log(tweets[i].text);
+          } 
+          
         });
 }
 
@@ -30,11 +32,11 @@ var theTwitter = function(){
 var theSpotify = function(){
 
           var spotify = new Spotify({
-            id: stuffINeed.spotify.access_token_key,
-            secret: stuffINeed.spotify.access_token_secret
+            id: stuffINeed.spotKey.id,
+            secret: stuffINeed.spotKey.secret
           });
   
-          fs.readFile("random.txt", "utf8", function(error, data) {
+          /**fs.readFile("random.txt", "utf8", function(error, data) {
           
             const trackSearch = dataArr;
               if (error) {
@@ -44,14 +46,16 @@ var theSpotify = function(){
               var dataArr = data.split(",");
               console.log(dataArr);
             
-          });
+          });**/
   
           
 
 
-          spotify.search({type: 'track', query: trackSearch }, function(err, data) {
+          spotify.search({type: 'track', query: arg2 }, function(err, data) {
               if (err) {
                 return console.log('Error occurred: ' + err);
+              } else {
+                console.log(data.tracks.items[0].album.artists[0].name);  
               }
           });
 }
@@ -61,6 +65,7 @@ var omdbReq = function(){
   var request = require("request");
   request("http://www.omdbapi.com/?t=" + arg2 + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
     if (!error && response.statusCode === 200) {
+      console.log(body)
       console.log("");
       console.log("");
       console.log("-------------------------------------------------------");
